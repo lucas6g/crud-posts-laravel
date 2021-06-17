9b3f5e5fd0e4e6512897c8a84b1673335ca75638
lucas6g <lucas.trabalho1533@gmail.com>
2021-06-17 02:14:31 -0300

    feat: add routes to api.php

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use \App\Http\Controllers\Api\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+*/
+
+
+Route::get('/user', [UserController::class, 'show']);
+
+Route::post('/user', [UserController::class, 'create']);
+
+
