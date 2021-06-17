c12dcf562a36236c8a3d25bd1c333c7da49f6c8f
lucas6g <lucas.trabalho1533@gmail.com>
2021-06-17 02:10:46 -0300

    test: add CreateUserService to register user

---
+++ tests/Unit/CreateUserServiceTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Unit;
+
+
+use App\Exceptions\AppError;
+use PHPUnit\Framework\TestCase;
+use App\Services\CreateUserService;
+use Tests\Unit\Fakes\Providers\FakeHashProvider;
+use Tests\Unit\Fakes\Repositories\FakeUserRepository;
+
+
+class CreateUserServiceTest extends TestCase
+{
+
+    protected $createUser;
+
+    public function setUp(): void
+    {
+        $this->createUser = new CreateUserService(
+            new FakeUserRepository(),
+            new FakeHashProvider()
+        );
+    }
+    /**
+     * @throws AppError
+     */
+    public function testCreateUser()
+    {
+
+
+       $user = $this->createUser->execute("anyName","anyEmail","anyPassword");
+       $this->assertEquals("anyName", $user->name);
+
+
+    }
+    /**
+     * @throws AppError
+     */
+    public function testNotCreateUserWhitSameEmail(){
+        $userEmail = "anyEmail";
+
+        $this->expectException(AppError::class);
+        $this->createUser->execute("anyName",$userEmail,"anyPassword");
+        $this->createUser->execute("anyName",$userEmail,"anyPassword");
+
+    }
+}
