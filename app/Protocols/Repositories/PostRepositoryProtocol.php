2d2174411f64d5e77cc108c57ca28f207d9dd3c0
lucas6g <lucas.trabalho1533@gmail.com>
2021-06-18 18:50:54 -0300

    feat : add post protocol for haddle post crud operations

---
+++ app/Protocols/Repositories/PostRepositoryProtocol.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Protocols\Repositories;
+
+use App\Models\Post;
+
+interface PostRepositoryProtocol{
+
+    function create($title,$content,$user_id,$img_url):Post;
+    function all():iterable;
+    function findById($post_id,$user_id):?Post;
+    function  update(Post $post):?Post;
+    function delete(Post $post):void;
+
+
+}
